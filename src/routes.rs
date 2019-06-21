ce0a887cb2120190548acfe2e6e2d683d7f9d814
Bazaah <github@luxolus.com>
2019-06-21 21:36:17 +0000

    Split responses into good/fail

--- src/routes.rs
+++ src/routes.rs
@@ -1,93 +1,126 @@
 use {
     crate::models::Raffle,
-    rocket::{http::Status, State},
+    rocket::{
+        http::Status,
+        request::Request,
+        response::{self, Responder, Response as rResponse},
+        State,
+    },
     rocket_contrib::json::Json,
     serde_json::value::Value as jVal,
     std::sync::RwLock,
 };
 
 type Internal<'r> = State<'r, RwLock<Raffle>>;
+type Response = Result<Good, Fail>;
 
 #[post("/ticket")]
-pub fn create_ticket(state: Internal) -> Result<Respond, Status> {
+pub fn create_ticket(state: Internal) -> Response {
     match state.write() {
         Ok(mut raffle) => {
             let ticket_id = raffle.new_ticket(None);
-            Ok(Respond::Ok(format!(
+            Ok(Good::Info(format!(
                 "Added ticket <{}> with [10] lines",
                 ticket_id
             )))
         }
-        Err(_) => Err(Status::InternalServerError),
+        Err(_) => Err(Fail::LockPoisoned),
     }
 }
 
 #[post("/ticket/<lines>")]
-pub fn create_ticket_with(state: Internal, lines: u64) -> Respond {
+pub fn create_ticket_with(state: Internal, lines: u64) -> Response {
     match state.write() {
         Ok(mut raffle) => {
             let ticket_id = raffle.new_ticket(Some(lines));
-            Respond::Ok(format!(
+            Ok(Good::Info(format!(
                 "Added ticket <{}> with [{}] lines",
                 ticket_id, lines
-            ))
+            )))
         }
-        Err(e) => Respond::LockPoisoned(format!("{}", e)),
+        Err(_) => Err(Fail::LockPoisoned),
     }
 }
 
 #[get("/ticket")]
-pub fn get_ticket_list(state: Internal) -> Respond {
+pub fn get_ticket_list(state: Internal) -> Response {
     match state.read() {
-        Ok(raffle) => Respond::Success(Json(raffle.get_ticket_list())),
-        Err(e) => Respond::LockPoisoned(format!("{}", e)),
+        Ok(raffle) => Ok(Good::Success(Json(raffle.get_ticket_list()))),
+        Err(_) => Err(Fail::LockPoisoned),
     }
 }
 
 #[get("/ticket/<id>")]
-pub fn get_ticket_from(state: Internal, id: u64) -> Respond {
+pub fn get_ticket_from(state: Internal, id: u64) -> Response {
     match state.read() {
         Ok(raffle) => match raffle.get_ticket(id) {
-            Ok(ticket) => Respond::Success(Json(ticket)),
-            Err(e) => Respond::Unprocessable(format!("{}", e)),
+            Ok(ticket) => Ok(Good::Success(Json(ticket))),
+            Err(e) => Err(Fail::Unprocessable(format!("{}", e))),
         },
-        Err(e) => Respond::LockPoisoned(format!("{}", e)),
+        Err(_) => Err(Fail::LockPoisoned),
     }
 }
 
 #[put("/ticket/<id>?<append>")]
-pub fn append_to_ticket(state: Internal, id: u64, append: Option<u64>) -> Respond {
+pub fn append_to_ticket(state: Internal, id: u64, append: Option<u64>) -> Response {
     match (append, state.write()) {
         (Some(lines), Ok(mut raffle)) => match raffle.append_ticket(id, lines) {
-            Ok(_) => Respond::Ok(format!("Appended [{}] lines to ticket <{}>", lines, id)),
-            Err(e) => Respond::Unprocessable(format!("{}", e)),
+            Ok(_) => Ok(Good::Info(format!(
+                "Appended [{}] lines to ticket <{}>",
+                lines, id
+            ))),
+            Err(e) => Err(Fail::Unprocessable(format!("{}", e))),
         },
-        (None, _) => Respond::BadRequest(format!("Malformed query: [append={{unsigned integer}}]")),
-        (_, Err(e)) => Respond::LockPoisoned(format!("{}", e)),
+        (None, _) => Err(Fail::BadRequest),
+        (_, Err(_)) => Err(Fail::LockPoisoned),
     }
 }
 
 #[put("/eval/<id>")]
-pub fn evaluate_ticket(state: Internal, id: u64) -> Respond {
+pub fn evaluate_ticket(state: Internal, id: u64) -> Response {
     match state.write() {
         Ok(mut raffle) => match raffle.evaluate_ticket(id) {
-            Ok(ticket) => Respond::Success(Json(ticket)),
-            Err(e) => Respond::Unprocessable(format!("{}", e)),
+            Ok(ticket) => Ok(Good::Success(Json(ticket))),
+            Err(e) => Err(Fail::Unprocessable(format!("{}", e))),
         },
-        Err(e) => Respond::LockPoisoned(format!("{}", e)),
+        Err(_) => Err(Fail::LockPoisoned),
     }
 }
 
 #[derive(Responder)]
-pub enum Respond {
+pub enum Good {
     #[response(status = 200)]
-    Ok(String),
+    Info(String),
     #[response(status = 200, content_type = "json")]
     Success(Json<jVal>),
-    #[response(status = 422)]
+}
+
+#[derive(Debug)]
+pub enum Fail {
     Unprocessable(String),
-    #[response(status = 400)]
-    BadRequest(String),
-    #[response(status = 500)]
-    LockPoisoned(String),
+    BadRequest,
+    LockPoisoned,
+}
+
+impl<'r> Responder<'r> for Fail {
+    fn respond_to(self, _: &Request) -> response::Result<'r> {
+        match self {
+            Fail::Unprocessable(err) => rResponse::build()
+                .sized_body(std::io::Cursor::new(err))
+                .status(Status::UnprocessableEntity)
+                .ok(),
+            Fail::BadRequest => rResponse::build()
+                .sized_body(std::io::Cursor::new(format!(
+                    "Malformed query: [append={{unsigned integer}}]"
+                )))
+                .status(Status::BadRequest)
+                .ok(),
+            Fail::LockPoisoned => rResponse::build()
+                .sized_body(std::io::Cursor::new(format!(
+                    "Unrecoverable error: Internal state poisoned, restart the server"
+                )))
+                .status(Status::ServiceUnavailable)
+                .ok(),
+        }
+    }
 }

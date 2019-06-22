061de0bcabaadc0e95570d32d01fbcac6bd1f218
Bazaah <github@luxolus.com>
2019-06-22 15:51:57 +0000

    added tests

--- src/models.rs
+++ src/models.rs
@@ -39,7 +39,6 @@ impl Raffle {
         };
 
         self.tickets.insert(self.count, ticket);
-        eprintln!("{:?}", &self.tickets);
         self.count
     }
 
@@ -49,7 +48,6 @@ impl Raffle {
             Some(ticket) => Ok(json!({"id": id, "lines": ticket.eval_list()})),
             None => {
                 let err = Err(ErrorKind::TicketNotFound(id));
-                eprintln!("in models {:?}", err);
                 err
             }
         }
@@ -62,7 +60,6 @@ impl Raffle {
             Some(ticket) => Ok(ticket.append(additional)),
             None => {
                 let err = Err(ErrorKind::TicketNotFound(id));
-                eprintln!("in models {:?}", err);
                 err
             }
         }
@@ -92,7 +89,6 @@ impl Raffle {
                 let list = ticket.eval_list();
                 let sum: u64 = list.iter().map(|i| *i as u64).sum();
                 let score = sum / list.len() as u64;
-                
                 Ok(Raffle::generate_response(id, score))
             }
             None => Err(ErrorKind::TicketNotFound(id)),
@@ -109,21 +105,21 @@ impl Raffle {
 
     fn generate_response(id: u64, score: u64) -> jVal {
         let response = match score {
-                    _n @ 0 => format_args!("you get nothing; good day sir!"),
-                    _n @ 1..=3 => format_args!("slightly better than a hostel shower!"),
-                    _n @ 4..=7 => format_args!("you're one of today's lucky 10,000!"),
-                    _n @ 8..=9 => format_args!("almost enough for a mediocre pizza!"),
-                    _ => format_args!("ding ding ding, you won the imaginary jackpot!"),
-                };
-                json!(format!(
-                    "For ticket {}, your score was {}... {}",
-                    id, score, response
-                ))
+            _n @ 0 => format_args!("you get nothing; good day sir!"),
+            _n @ 1..=3 => format_args!("slightly better than a hostel shower!"),
+            _n @ 4..=7 => format_args!("you're one of today's lucky 10,000!"),
+            _n @ 8..=9 => format_args!("almost enough for a mediocre pizza!"),
+            _ => format_args!("ding ding ding, you won the imaginary jackpot!"),
+        };
+        json!(format!(
+            "For ticket {}, your score was {}... {}",
+            id, score, response
+        ))
     }
 }
 
 // Internal representation of a Ticket
-#[derive(Clone, Serialize, Debug)]
+#[derive(Clone, Serialize, Debug, PartialEq)]
 struct Ticket {
     line_list: Vec<Line>,
 }
@@ -188,7 +184,7 @@ impl fmt::Display for Ticket {
 }
 
 // Named tuple which holds 3 numbers between 0 and 2: [0,1,2]
-#[derive(Debug, Clone, Copy, Serialize)]
+#[derive(Debug, Clone, Copy, PartialEq, Serialize)]
 struct Line(u8, u8, u8);
 
 impl Line {
@@ -229,4 +225,290 @@ impl fmt::Display for ErrorKind {
             ErrorKind::TicketNotFound(id) => write!(f, "Ticket id: {} doesn't exist", id),
         }
     }
+}
+
+/*
+Code
+-------------------------------------------------------------------------------
+Tests
+*/
+
+#[cfg(test)]
+mod tests {
+    #![allow(non_snake_case)]
+    use super::*;
+
+    macro_rules! static_ticket {
+        () => {{
+            let line_list = LINE_SEED_VALUES
+                .iter()
+                .map(|i| Line::from(*i))
+                .collect::<Vec<Line>>();
+            let ticket = Ticket { line_list };
+            ticket
+        }};
+    }
+
+    // ErrorKind tests
+    #[test]
+    fn TicketNotFound_display() {
+        assert_eq!(
+            format!("{}", ErrorKind::TicketNotFound(42)),
+            "Ticket id: 42 doesn't exist"
+        )
+    }
+
+    // Line tests
+
+    #[test]
+    fn Line_from() {
+        LINE_SEED_VALUES
+            .iter()
+            .for_each(|seed| assert_eq!(Line::from(*seed), Line(seed.0, seed.1, seed.2)));
+    }
+
+    #[test]
+    fn Line_eval_line() {
+        LINE_SEED_VALUES
+            .iter()
+            .zip(LINE_EVAL_VALUES.iter())
+            .map(|(seed, value)| (Line::from(*seed).eval_line(), *value))
+            .for_each(|(eval, value)| assert_eq!(eval, value))
+    }
+
+    #[test]
+    fn Line_display() {
+        let line = Line::from(LINE_SEED_VALUES[0]);
+        let evaluated = LINE_EVAL_VALUES[0];
+
+        assert_eq!(format!("{}", line), format!("|{}|", evaluated));
+    }
+
+    // Ticket tests
+    #[test]
+    fn Ticket_new() {
+        let ticket = Ticket::new();
+        let ticket2 = Ticket::new();
+        // It is theoretically possible for this test to fail due the inherent randomness;
+        // therefore check against a third rand generation, before failing the test
+        assert!(ticket != ticket2 || ticket != Ticket::new())
+    }
+
+    #[test]
+    fn Ticket_from() {
+        (1..=10).for_each(|lines_num| {
+            assert_eq!(Ticket::from(lines_num).line_list.len(), lines_num as usize)
+        })
+    }
+
+    #[test]
+    fn Ticket_append() {
+        let start_size = 10;
+        let additional_lines = 7;
+        let end_size = 17;
+        assert_eq!(start_size + additional_lines, end_size);
+
+        let mut ticket = Ticket::from(start_size);
+        ticket.append(additional_lines);
+        assert_eq!(ticket.line_list.len(), end_size as usize);
+    }
+
+    #[test]
+    fn Ticket_eval_list() {
+        let line_list: Vec<Line> = LINE_SEED_VALUES.iter().map(|i| Line::from(*i)).collect();
+        let ticket = Ticket { line_list };
+        assert_eq!(
+            ticket.eval_list(),
+            LINE_EVAL_VALUES.iter().map(|i| *i).collect::<Vec<u8>>()
+        );
+    }
+    #[test]
+    fn Ticket_display() {
+        let line_list: Vec<Line> = LINE_SEED_VALUES
+            .iter()
+            .take(10)
+            .map(|i| Line::from(*i))
+            .collect();
+        let ticket = Ticket { line_list };
+        let evals: Vec<u8> = LINE_EVAL_VALUES.iter().take(10).map(|i| *i).collect();
+        assert_eq!(ticket.line_list.len(), evals.len());
+
+        assert_eq!(
+            format!("{}", ticket),
+            format!(
+                "[|{}| |{}| |{}| |{}| |{}| |{}| |{}| |{}| |{}| |{}|]",
+                evals[0],
+                evals[1],
+                evals[2],
+                evals[3],
+                evals[4],
+                evals[5],
+                evals[6],
+                evals[7],
+                evals[8],
+                evals[9],
+            )
+        )
+    }
+
+    // Raffle tests
+    #[test]
+    fn Raffle_instantiate() {
+        let base_raffle = Raffle::instantiate();
+
+        assert!(base_raffle.count == 0 && base_raffle.tickets.is_empty());
+    }
+
+    #[test]
+    fn Raffle_new_ticket_default() {
+        let mut raffle = Raffle::instantiate();
+        let ticket_id = raffle.new_ticket(None);
+
+        assert!(raffle.count == 1 && raffle.tickets.len() == 1 && ticket_id == 1)
+    }
+
+    #[test]
+    fn Raffle_new_ticket_with_lines() {
+        let mut raffle = Raffle::instantiate();
+        let len80 = 80;
+        let ticket_id = raffle.new_ticket(Some(len80));
+
+        assert!(
+            raffle.count == 1
+                && raffle.tickets.len() == 1
+                && ticket_id == *raffle.tickets.keys().nth(0).unwrap()
+                && len80 as usize == raffle.tickets.get(&ticket_id).unwrap().line_list.len()
+        )
+    }
+
+    #[test]
+    fn Raffle_get_ticket_success() {
+        let mut raffle = Raffle::instantiate();
+        raffle.new_ticket(None);
+        let existing_id = 1;
+
+        assert!(raffle.get_ticket(existing_id).is_ok())
+    }
+
+    #[test]
+    fn Raffle_get_ticket_fail() {
+        let mut raffle = Raffle::instantiate();
+        raffle.new_ticket(None);
+        let nonexistent_id = 100;
+
+        assert!(raffle.get_ticket(nonexistent_id).is_err())
+    }
+
+    #[test]
+    fn Raffle_append_ticket_success() {
+        let mut raffle = Raffle::instantiate();
+        raffle.new_ticket(None);
+        let existing_id = 1;
+
+        assert!(raffle.append_ticket(existing_id, 10).is_ok())
+    }
+
+    #[test]
+    fn Raffle_append_ticket_fail() {
+        let mut raffle = Raffle::instantiate();
+        let nonexistent_id = 100;
+
+        assert!(raffle.append_ticket(nonexistent_id, 10).is_err())
+    }
+
+    #[test]
+    fn Raffle_evaluate_ticket_success() {
+        let mut raffle = Raffle::instantiate();
+        let ticket = static_ticket!();
+        assert!(raffle.tickets.insert(1, ticket).is_none());
+
+        assert!(raffle.evaluate_ticket(1).is_ok())
+    }
+
+    #[test]
+    fn Raffle_evaluate_ticket_failure() {
+        let mut raffle = Raffle::instantiate();
+        let ticket = static_ticket!();
+        assert!(raffle.tickets.insert(1, ticket).is_none());
+
+        assert!(raffle.evaluate_ticket(2).is_err())
+    }
+
+    #[test]
+    fn Raffle_get_ticket_list_type() {
+        let raffle = Raffle::instantiate();
+        assert!(raffle.get_ticket_list().is_array())
+    }
+
+    #[test]
+    fn Raffle_get_ticket_list_composition() {
+        let mut raffle = Raffle::instantiate();
+        let ticket = static_ticket!();
+        let idx = 1;
+        assert!(raffle.tickets.insert(idx, ticket).is_none());
+        let output: Vec<u8> = LINE_EVAL_VALUES.iter().map(|i| *i).collect();
+
+        assert_eq!(
+            raffle.get_ticket_list(),
+            json!([{"id": idx, "lines": output}])
+        );
+    }
+
+    // Test data
+    static LINE_SEED_VALUES: [(u8, u8, u8); 50] = [
+        (1, 0, 2),
+        (2, 2, 2),
+        (0, 0, 0),
+        (1, 1, 2),
+        (0, 1, 0),
+        (0, 2, 2),
+        (0, 2, 1),
+        (2, 2, 2),
+        (1, 1, 1),
+        (2, 2, 2),
+        (0, 2, 1),
+        (2, 1, 1),
+        (1, 0, 2),
+        (2, 2, 0),
+        (1, 2, 0),
+        (0, 0, 2),
+        (1, 1, 0),
+        (1, 1, 1),
+        (1, 0, 1),
+        (2, 0, 1),
+        (0, 0, 1),
+        (2, 2, 0),
+        (0, 0, 1),
+        (0, 0, 0),
+        (2, 0, 0),
+        (0, 2, 0),
+        (1, 0, 1),
+        (1, 1, 2),
+        (2, 1, 1),
+        (1, 2, 1),
+        (0, 2, 1),
+        (0, 1, 1),
+        (0, 2, 0),
+        (0, 2, 0),
+        (1, 2, 2),
+        (1, 0, 2),
+        (0, 0, 0),
+        (2, 1, 1),
+        (1, 1, 0),
+        (1, 1, 1),
+        (1, 2, 1),
+        (2, 1, 1),
+        (2, 1, 2),
+        (0, 1, 1),
+        (0, 0, 2),
+        (1, 2, 2),
+        (2, 0, 2),
+        (0, 0, 0),
+        (0, 2, 0),
+        (0, 2, 0),
+    ];
+    static LINE_EVAL_VALUES: [u8; 50] = [
+        1, 5, 5, 0, 0, 1, 1, 5, 5, 5, 1, 1, 1, 0, 1, 10, 10, 5, 10, 1, 0, 0, 0, 5, 10, 10, 10, 0,
+        1, 0, 1, 10, 10, 10, 1, 1, 5, 1, 10, 5, 0, 1, 0, 10, 10, 1, 0, 5, 10, 10,
+    ];
 }
